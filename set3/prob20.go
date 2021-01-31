2591b9865c704f07e544e175bc6153a2dd4e5fce
J. O. Makar <jomakar@gmail.com>
2021-01-30 22:24:26 -0500

    set3 prob20 rewrite

--- set3/prob20.go
+++ set3/prob20.go
@@ -5,6 +5,7 @@ import (
 	"../set1/base64"
 	"../set1/score"
 
+	"bufio"
 	"bytes"
 	"errors"
 	"fmt"
@@ -19,10 +20,6 @@ import (
 )
 
 func main() {
-	// The intention of this exercise was to substitute plaintext letters (then digrams, trigrams, ...)  based on probabilities.
-	// When I first solved this problem the following was the solution that initially came to mind which is actually the next problem solution.
-	// TODO Solve this as it was originally intended, start with a semi-automated, interactive approach.
-
 	//
 	// Encrypt the plaintexts
  	//
@@ -34,47 +31,27 @@ func main() {
 		return b
 	}
 
-	plaintexts := []string{
-		"SSBoYXZlIG1ldCB0aGVtIGF0IGNsb3NlIG9mIGRheQ==",
-		"Q29taW5nIHdpdGggdml2aWQgZmFjZXM=",
-		"RnJvbSBjb3VudGVyIG9yIGRlc2sgYW1vbmcgZ3JleQ==",
-		"RWlnaHRlZW50aC1jZW50dXJ5IGhvdXNlcy4=",
-		"SSBoYXZlIHBhc3NlZCB3aXRoIGEgbm9kIG9mIHRoZSBoZWFk",
-		"T3IgcG9saXRlIG1lYW5pbmdsZXNzIHdvcmRzLA==",
-		"T3IgaGF2ZSBsaW5nZXJlZCBhd2hpbGUgYW5kIHNhaWQ=",
-		"UG9saXRlIG1lYW5pbmdsZXNzIHdvcmRzLA==",
-		"QW5kIHRob3VnaHQgYmVmb3JlIEkgaGFkIGRvbmU=",
-		"T2YgYSBtb2NraW5nIHRhbGUgb3IgYSBnaWJl",
-		"VG8gcGxlYXNlIGEgY29tcGFuaW9u",
-		"QXJvdW5kIHRoZSBmaXJlIGF0IHRoZSBjbHViLA==",
-		"QmVpbmcgY2VydGFpbiB0aGF0IHRoZXkgYW5kIEk=",
-		"QnV0IGxpdmVkIHdoZXJlIG1vdGxleSBpcyB3b3JuOg==",
-		"QWxsIGNoYW5nZWQsIGNoYW5nZWQgdXR0ZXJseTo=",
-		"QSB0ZXJyaWJsZSBiZWF1dHkgaXMgYm9ybi4=",
-		"VGhhdCB3b21hbidzIGRheXMgd2VyZSBzcGVudA==",
-		"SW4gaWdub3JhbnQgZ29vZCB3aWxsLA==",
-		"SGVyIG5pZ2h0cyBpbiBhcmd1bWVudA==",
-		"VW50aWwgaGVyIHZvaWNlIGdyZXcgc2hyaWxsLg==",
-		"V2hhdCB2b2ljZSBtb3JlIHN3ZWV0IHRoYW4gaGVycw==",
-		"V2hlbiB5b3VuZyBhbmQgYmVhdXRpZnVsLA==",
-		"U2hlIHJvZGUgdG8gaGFycmllcnM/",
-		"VGhpcyBtYW4gaGFkIGtlcHQgYSBzY2hvb2w=",
-		"QW5kIHJvZGUgb3VyIHdpbmdlZCBob3JzZS4=",
-		"VGhpcyBvdGhlciBoaXMgaGVscGVyIGFuZCBmcmllbmQ=",
-		"V2FzIGNvbWluZyBpbnRvIGhpcyBmb3JjZTs=",
-		"SGUgbWlnaHQgaGF2ZSB3b24gZmFtZSBpbiB0aGUgZW5kLA==",
-		"U28gc2Vuc2l0aXZlIGhpcyBuYXR1cmUgc2VlbWVkLA==",
-		"U28gZGFyaW5nIGFuZCBzd2VldCBoaXMgdGhvdWdodC4=",
-		"VGhpcyBvdGhlciBtYW4gSSBoYWQgZHJlYW1lZA==",
-		"QSBkcnVua2VuLCB2YWluLWdsb3Jpb3VzIGxvdXQu",
-		"SGUgaGFkIGRvbmUgbW9zdCBiaXR0ZXIgd3Jvbmc=",
-		"VG8gc29tZSB3aG8gYXJlIG5lYXIgbXkgaGVhcnQs",
-		"WWV0IEkgbnVtYmVyIGhpbSBpbiB0aGUgc29uZzs=",
-		"SGUsIHRvbywgaGFzIHJlc2lnbmVkIGhpcyBwYXJ0",
-		"SW4gdGhlIGNhc3VhbCBjb21lZHk7",
-		"SGUsIHRvbywgaGFzIGJlZW4gY2hhbmdlZCBpbiBoaXMgdHVybiw=",
-		"VHJhbnNmb3JtZWQgdXR0ZXJseTo=",
-		"QSB0ZXJyaWJsZSBiZWF1dHkgaXMgYm9ybi4=",
+	var basePath string 
+	if _, p, _, ok := runtime.Caller(0); !ok {
+		log.Panic(errors.New("runtime.Caller() failed"))
+	} else {
+		basePath = filepath.Dir(p)
+	}
+
+	inputFile, err := os.Open(filepath.Join(basePath, "prob20.txt"))
+	if err != nil {
+		log.Panic(err)
+	}
+	defer inputFile.Close()
+
+	var plaintexts [][]byte
+	scanner := bufio.NewScanner(inputFile)
+	for scanner.Scan() {
+		plaintext, err := base64.Decode(scanner.Text())
+		if err != nil {
+			log.Panic(err)
+		}
+		plaintexts = append(plaintexts, plaintext)
 	}
 
 	key := randBytes(aes.BlockSize)
@@ -82,12 +59,7 @@ func main() {
 
 	ciphertexts := make([][]byte, len(plaintexts))
 	for i := 0; i < len(plaintexts); i++ {
-		decoded, err := base64.Decode(plaintexts[i])
-		if err != nil {
-			log.Panic(err)
-		}
-
-		ciphertexts[i], err = aes.CtrEncrypt(decoded, key, nonce)
+		ciphertexts[i], err = aes.CtrEncrypt(plaintexts[i], key, nonce)
 		if err != nil {
 			log.Panic(err)
 		}
@@ -172,12 +144,7 @@ func main() {
 			derivedPlaintext[j] = ciphertexts[i][j] ^ keystream[j]
 		}
 
-		actualPlaintext, err := base64.Decode(plaintexts[i])
-		if err != nil {
-			log.Panic(err)
-		}
-
-		log.Printf("%d: %q %q", i, derivedPlaintext, actualPlaintext)
+		log.Printf("%d: %q %q", i, derivedPlaintext, plaintexts[i])
 	}
 
 	// The corpus used (The Brothers Karamazov) to generate the scoring table is long;
